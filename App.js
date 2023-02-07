3417aa8399dab71ba78d85690fd725ae933b56f7
aaronByrne1 <65355719+aaronByrne1@users.noreply.github.com>
2023-02-07 17:13:40 +0100

    Added firebase auth and created folder that has 2 screens in it to navigate

--- App.js
+++ App.js
@@ -1,46 +1,22 @@
-import { StatusBar } from "expo-status-bar";
-import { Text, View, Image, TextInput } from "react-native";
-import { Button } from "react-native-paper";
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Signin from "./screens/signin";
+import Dashboard from "./screens/dashboard";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
-      <View className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-        <View className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-          <View>
-            <Text
-              htmlFor="email"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Email address
-            </Text>
-            <View className="mt-1">
-              <TextInput className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" />
-            </View>
-          </View>
-
-          <View>
-            <Text className="block text-sm font-medium text-gray-700">
-              Password
-            </Text>
-            <View className="mt-1">
-              <TextInput className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" />
-            </View>
-          </View>
-
-          <View>
-            <Button
-              mode="contained"
-              className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-            >
-              Sign in
-            </Button>
-          </View>
-
-          <View className="mt-6"></View>
-        </View>
-      </View>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="Signin" component={Signin} />
+        <Stack.Screen name="Dashboard" component={Dashboard} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
---
+++ firebase.js
@@ -0,0 +1,44 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+  signOut,
+} from "firebase/auth";
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+  FIREBASE_MEASUREMENT_ID,
+} from "@env";
+
+const firebaseConfig = {
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const logInWithEmailAndPassword = async (email, password) => {
+  try {
+    const res = await signInWithEmailAndPassword(auth, email, password);
+    console.log(res.user.uid);
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+const logout = () => {
+  signOut(auth);
+};
+export { auth, logInWithEmailAndPassword, logout };
