f37088d2b1a2ae7680d93df612d569d35df12d91
aaronByrne1 <65355719+aaronByrne1@users.noreply.github.com>
2023-02-16 01:06:31 +0100

    Ready to integrate and popu;ate users into the mongoDB database

--- firebase.js
+++ firebase.js
@@ -15,7 +15,7 @@ import {
   FIREBASE_APP_ID,
   FIREBASE_MEASUREMENT_ID,
 } from "@env";
-
+import axios from "axios";
 const firebaseConfig = {
   apiKey: FIREBASE_API_KEY,
   authDomain: FIREBASE_AUTH_DOMAIN,
@@ -32,6 +32,8 @@ const auth = getAuth(app);
 const logInWithEmailAndPassword = async (email, password) => {
   try {
     const res = await signInWithEmailAndPassword(auth, email, password);
+    const res3 = await axios.get("http://192.168.170.179:5000");
+    console.log(res3.data);
     console.log(res.user.uid);
   } catch (err) {
     console.error(err);
@@ -42,6 +44,13 @@ const registerWithEmailAndPassword = async (name, email, password) => {
   try {
     const res = await createUserWithEmailAndPassword(auth, email, password);
     const user = res.user;
+    //axios post to the backend with body containing user.uid and name
+    const res2 = await axios.post("http://192.168.170.179:5000/members/", {
+      memberID: user.uid,
+      name: name,
+      gymOwner: "7pDwFT9gTMU1uil4WlrbrTlvxsp2",
+    });
+    console.log(res2);
   } catch (err) {
     console.error(err);
     alert(err.message);
