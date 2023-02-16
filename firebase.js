8a9044f4abac2847365959fe618d378c391a5a5d
aaronByrne1 <65355719+aaronByrne1@users.noreply.github.com>
2023-02-16 09:34:37 +0000

    Added ipAddress for mac

--- firebase.js
+++ firebase.js
@@ -45,7 +45,7 @@ const registerWithEmailAndPassword = async (name, email, password) => {
     const res = await createUserWithEmailAndPassword(auth, email, password);
     const user = res.user;
     //axios post to the backend with body containing user.uid and name
-    const res2 = await axios.post("http://192.168.170.179:5000/members/", {
+    const res2 = await axios.post("http://10.6.6.176:5000/members/", {
       memberID: user.uid,
       name: name,
       gymOwner: "7pDwFT9gTMU1uil4WlrbrTlvxsp2",
