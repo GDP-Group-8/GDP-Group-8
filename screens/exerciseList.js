337245f40d0e82f90d47387e4151b57b7f8b016e
aaronByrne1 <65355719+aaronByrne1@users.noreply.github.com>
2023-02-22 12:53:29 +0100

    Added an on press function to the list

--- screens/exerciseList.js
+++ screens/exerciseList.js
@@ -1,100 +1,92 @@
 import React, { useState } from "react";
-import { View,ScrollView } from "react-native";
-import { Text,Divider,List,Headline} from "react-native-paper"
-export default function ExerciseList({navigation}){
+import { View, ScrollView } from "react-native";
+import { Text, Divider, List, Headline, Button } from "react-native-paper";
+export default function ExerciseList({ navigation }) {
+  const [exercises, setExercises] = React.useState([
+    {
+      name: "Yoga",
+      desc: "Relax the body and mind, relieve the mood",
+    },
+    {
+      name: "Sit-ups",
+      desc: "High intensity fat burning and muscle strengthening",
+    },
+    {
+      name: "Jogging",
+      desc: "Improve body endurance",
+    },
+    {
+      name: "Yoga",
+      desc: "Relax the body and mind, relieve the mood",
+    },
+    {
+      name: "Sit-ups",
+      desc: "High intensity fat burning and muscle strengthening",
+    },
+    {
+      name: "Jogging",
+      desc: "Improve body endurance",
+    },
+    {
+      name: "Yoga",
+      desc: "Relax the body and mind, relieve the mood",
+    },
+    {
+      name: "Sit-ups",
+      desc: "High intensity fat burning and muscle strengthening",
+    },
+    {
+      name: "Jogging",
+      desc: "Improve body endurance",
+    },
+    {
+      name: "Yoga",
+      desc: "Relax the body and mind, relieve the mood",
+    },
+    {
+      name: "Sit-ups",
+      desc: "High intensity fat burning and muscle strengthening",
+    },
+    {
+      name: "Jogging",
+      desc: "Improve body endurance",
+    },
+    {
+      name: "Yoga",
+      desc: "Relax the body and mind, relieve the mood",
+    },
+    {
+      name: "Sit-ups",
+      desc: "High intensity fat burning and muscle strengthening",
+    },
+    {
+      name: "Jogging",
+      desc: "Improve body endurance",
+    },
+  ]);
 
-
-    const [exercises,setExercises] = React.useState([
-        {
-            name:"Yoga",
-            desc:"Relax the body and mind, relieve the mood"
-        },
-        {
-            name:"Sit-ups",
-            desc:"High intensity fat burning and muscle strengthening"
-        },
-        {
-            name:"Jogging",
-            desc:"Improve body endurance"
-        },
-        {
-            name:"Yoga",
-            desc:"Relax the body and mind, relieve the mood"
-        },
-        {
-            name:"Sit-ups",
-            desc:"High intensity fat burning and muscle strengthening"
-        },
-        {
-            name:"Jogging",
-            desc:"Improve body endurance"
-        },
-        {
-            name:"Yoga",
-            desc:"Relax the body and mind, relieve the mood"
-        },
-        {
-            name:"Sit-ups",
-            desc:"High intensity fat burning and muscle strengthening"
-        },
-        {
-            name:"Jogging",
-            desc:"Improve body endurance"
-        },
-        {
-            name:"Yoga",
-            desc:"Relax the body and mind, relieve the mood"
-        },
-        {
-            name:"Sit-ups",
-            desc:"High intensity fat burning and muscle strengthening"
-        },
-        {
-            name:"Jogging",
-            desc:"Improve body endurance"
-        },
-        {
-            name:"Yoga",
-            desc:"Relax the body and mind, relieve the mood"
-        },
-        {
-            name:"Sit-ups",
-            desc:"High intensity fat burning and muscle strengthening"
-        },
-        {
-            name:"Jogging",
-            desc:"Improve body endurance"
-        },
-        
-    ]);
-    
-    return (
-        <View className = "bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-        <Headline style={{"textAlign":"center"}}>Exercises List</Headline>
-        <ScrollView >
-            {
-                exercises.map((exercise,index)=>{
-                        return (
-                            <View>
-                                
-                                <View className="mt-1">
-                                    <List.Item
-                                        
-                                        title={exercise.name}
-                                        description={exercise.desc}
-                                        left={props => <List.Icon color="orange"  icon="alpha-e-box" />}
-                                    />
-                                </View>
-                                <Divider />
-                            </View>
-                            
-                        )
-                        })
-            }
-            
-        </ScrollView>
-        </View>
-        
-        
-      );
+  return (
+    <View className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+      <Headline style={{ textAlign: "center" }}>Exercises List</Headline>
+      <ScrollView>
+        {exercises.map((exercise, index) => {
+          return (
+            <View>
+              <View className="mt-1">
+                <List.Item
+                  title={exercise.name}
+                  description={exercise.desc}
+                  left={(props) => (
+                    <List.Icon color="orange" icon="alpha-e-box" />
+                  )}
+                  onPress={() => console.log("Pressed")}
+                />
+              </View>
+              <Divider />
+            </View>
+          );
+        })}
+      </ScrollView>
+    </View>
+  );
 }
